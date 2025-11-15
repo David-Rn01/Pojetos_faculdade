033d146c7ba34c4b938ddeecacf22f51aca4e40c
vitorsantana <matrixesperiente@gmail.com>
2025-11-15 11:09:01 -0300

    modified:   faculdade/fleshtech/src/pages/_app.tsx

--- faculdade/fleshtech/src/pages/_app.tsx
+++ faculdade/fleshtech/src/pages/_app.tsx
@@ -1,5 +1,5 @@
 // src/pages/_app.tsx
-//import '@/styles/globals.css';
+import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
