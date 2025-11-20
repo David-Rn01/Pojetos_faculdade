373bafb40a5b87f379de88c95b6031a8636897c2
vitorsantana <matrixesperiente@gmail.com>
2025-11-19 21:46:53 -0300

    lionks Update product images to new URLs

--- src/data/products.ts
+++ src/data/products.ts
@@ -17,7 +17,7 @@ export const mockProducts: Product[] = [
     id: '1', 
     name: 'Smartphone Pro X (128GB)', 
     price: 4299.90, 
-    image: 'https://m.media-amazon.com/images/I/61kE-oxS5QL._AC_SX522_.jpg',
+    image: 'https://www.magazineluiza.com.br/samsung-galaxy-s25-ultra-5g-256gb-galaxy-ai-titanio-azul-69-12gb-ram-cam-quadrupla-200-50-10-50mp-bateria-5000mah-dual-chip/p/238920600/te/g25u/?&seller_id=magazineluiza&utm_source=google&utm_medium=cpc&utm_term=79684&utm_campaign=google_eco_per_ven_pla_mob_apo_1p_te-csp&utm_content=&partner_id=79684&gclsrc=aw.ds&gad_source=1&gad_campaignid=22578732921&gbraid=0AAAAAD4zZmSb5jRtGR85sdAqNSJdXMUlc&gclid=CjwKCAiA8vXIBhAtEiwAf3B-g-abF9Q0yRqyD-b9Pe6Y0eOhXGceW76806KLoHmLjzMs2a7hw1Vo-RoCMkUQAvD_BwE',
     images: [
         'https://m.media-amazon.com/images/I/61kE-oxS5QL._AC_SX522_.jpg',
         'https://m.media-amazon.com/images/I/81I-Gk-S8qL._AC_SL1500_.jpg',
@@ -38,7 +38,7 @@ export const mockProducts: Product[] = [
     id: '2', 
     name: 'Notebook UltraSlim Flashtech', 
     price: 7599.00, 
-    image: 'https://m.media-amazon.com/images/I/71jG+e7roXL._AC_SL1500_.jpg',
+    image: 'https://www.magazineluiza.com.br/macbook-air-apple-13-chip-m4-cpu-10-nucleos-gpu-8-nucleos-neural-engine-16-nucleos-16gb-ram-ssd-256gb-meia-noite-mw123bz-a/p/bee45d2jg5/in/mack/?seller_id=kabum',
     images: [
         'https://m.media-amazon.com/images/I/71jG+e7roXL._AC_SL1500_.jpg',
         'https://m.media-amazon.com/images/I/7106jNnZa+L._AC_SL1500_.jpg',
@@ -58,7 +58,7 @@ export const mockProducts: Product[] = [
     id: '3', 
     name: 'Fone de Ouvido Flashtech Bass', 
     price: 899.90, 
-    image: 'https://m.media-amazon.com/images/I/61v02CJ6cJL._AC_SL1500_.jpg',
+    image: 'https://www.magazineluiza.com.br/airpods-4-apple-com-estojo-de-recarga-usb-c-branco-mxp63bz-a/p/dfa1g7kfb4/ea/aair/?seller_id=kabum',
     images: [
         'https://m.media-amazon.com/images/I/61v02CJ6cJL._AC_SL1500_.jpg',
         'https://m.media-amazon.com/images/I/719hDk8t0iL._AC_SL1500_.jpg',
@@ -77,7 +77,7 @@ export const mockProducts: Product[] = [
     id: '4', 
     name: 'Smartwatch 2.0 GPS', 
     price: 1899.50, 
-    image: 'https://m.media-amazon.com/images/I/61s4-e5v0ZL._AC_SL1500_.jpg',
+    image: 'https://www.magazineluiza.com.br/apple-watch-se-3-gps-caixa-estelar-de-aluminio-40mm-pulseira-esportiva-estelar-p-m/p/240598400/te/wats/?seller_id=magazineluiza',
     images: [
         'https://m.media-amazon.com/images/I/61s4-e5v0ZL._AC_SL1500_.jpg',
         'https://m.media-amazon.com/images/I/71L-l1jK1dL._AC_SL1500_.jpg',
@@ -96,7 +96,7 @@ export const mockProducts: Product[] = [
     id: '5', 
     name: 'Carregador Rápido 65W', 
     price: 299.00, 
-    image: 'https://m.media-amazon.com/images/I/51j3fP-y6dL._AC_SL1000_.jpg',
+    image: 'https://www.magazineluiza.com.br/carregador-usb-c-certificado-pela-apple-pacote-de-2-m-para-iphone-lightning-kruidvat/p/kb5h0j5c6d/te/spcl/?seller_id=nocnocestadosunidos',
     images: [
         'https://m.media-amazon.com/images/I/51j3fP-y6dL._AC_SL1000_.jpg',
         'https://m.media-amazon.com/images/I/61g+w0Wj-oL._AC_SL1500_.jpg'
@@ -114,7 +114,7 @@ export const mockProducts: Product[] = [
     id: '6', 
     name: 'Mouse Gamer Pro', 
     price: 499.00, 
-    image: 'https://m.media-amazon.com/images/I/61mpMH5TzkL._AC_SL1500_.jpg',
+    image: 'https://www.magazineluiza.com.br/mouse-gamer-sem-fio-logitech-g-pro-x-superlight-2-dex-lightspeed-44-000-dpi-design-assimetrico-para-destros-branco-910-007364/p/dd55f17b6e/in/rato/?seller_id=kabum',
     images: [
         'https://m.media-amazon.com/images/I/61mpMH5TzkL._AC_SL1500_.jpg',
         'https://m.media-amazon.com/images/I/71b3O-XaD+L._AC_SL1500_.jpg',
