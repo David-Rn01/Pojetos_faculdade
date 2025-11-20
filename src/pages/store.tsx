eb80a63f90a4bd9e40a816a70cf4f30f9f69f1c3
Kawan-Karlos <kawansenaa@gmail.com>
2025-11-19 21:29:43 -0300

    Alteração aceita

--- src/pages/store.tsx
+++ src/pages/store.tsx
@@ -6,17 +6,10 @@ import ProductCard from '../components/ProductCard'; // Usando caminho relativo
 // --- DADOS FICTÍCIOS (MOCK) ---
 // Este é o formato que o seu ProductCard.tsx espera
 const mockProducts = [
-<<<<<<< HEAD
   { id: '1', name: 'Samsung Galaxy S25 Ultra', price: 7649.00, image: 'https://m.magazineluiza.com.br/a-static/420x420/samsung-galaxy-s25-ultra-5g-256gb-galaxy-ai-titanio-azul-69-12gb-ram-cam-quadrupla-200-50-10-50mp-bateria-5000mah-dual-chip/magazineluiza/238920600/ff9276f61022e47dcf89b5f0031cec0b.jpg', colors: ['Preto', 'Prata'], category: 'Smartphones' },
   { id: '2', name: 'Notebook UltraSlim Flashtech', price: 7599.00, image: 'https://placehold.co/600x600/1e293b/94a3b8?text=Notebook', colors: ['Cinza', 'Prata'], category: 'Notebooks' },
   { id: '3', name: 'Fone de Ouvido Sem Fio Flashtech Bass', price: 899.90, image: 'https://placehold.co/600x600/1e293b/94a3b8?text=Fone', colors: ['Preto', 'Branco'], category: 'Acessórios' },
   { id: '4', name: 'Smartwatch 2.0 GPS', price: 1899.50, image: 'https://placehold.co/600x600/1e293b/94a3b8?text=Watch', colors: ['Preto', 'Dourado'], category: 'Acessórios' },
-=======
-  { id: '1', name: 'Smartphone Pro X (128GB, Tela 6.7")', price: 4299.90, image: 'https://m.media-amazon.com/images/I/61kE-oxS5QL._AC_SX522_.jpg', colors: ['Preto', 'Prata'], category: 'Smartphones' },
-  { id: '2', name: 'Notebook UltraSlim Flashtech', price: 7599.00, image: 'https://m.media-amazon.com/images/I/51LAU1+ACrL._AC_SX679_.jpg', colors: ['Cinza', 'Prata'], category: 'Notebooks' },
-  { id: '3', name: 'Fone de Ouvido Sem Fio Flashtech Bass', price: 899.90, image: 'https://m.media-amazon.com/images/I/51+i1WUqFLL._AC_SX522_.jpg', colors: ['Preto', 'Branco'], category: 'Acessórios' },
-  { id: '4', name: 'Smartwatch 2.0 GPS', price: 1899.50, image: 'https://m.media-amazon.com/images/I/61e3-n3lLcL._AC_SY300_SX300_QL70_ML2_.jpg', colors: ['Preto', 'Dourado'], category: 'Acessórios' },
->>>>>>> f04069e6423c3dc7d1b77a9570af34263c395356
   { id: '5', name: 'Carregador Rápido 65W', price: 299.00, image: 'https://placehold.co/600x600/1e293b/94a3b8?text=Carregador', colors: ['Branco'], category: 'Outros' },
   { id: '6', name: 'Mouse Gamer Pro', price: 499.00, image: 'https://placehold.co/600x600/1e293b/94a3b8?text=Mouse', colors: ['Preto', 'RGB'], category: 'Periféricos' },
   { id: '7', name: 'Monitor UltraWide 34"', price: 2799.00, image: 'https://placehold.co/600x600/1e293b/94a3b8?text=Monitor', colors: ['Preto'], category: 'Monitores' },
